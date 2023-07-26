a007d71ee6d0197b434fa3f470ba5bbc9a0ed7d4
gupta-aayushkr <gupta.aayushkr@gmail.com>
2023-07-26 18:50:06 +0530

    first commit

---
+++ t1.py
@@ -0,0 +1,21 @@
+from turtle import *
+
+# fd(120)
+# lt(60)
+# fd(120)
+# lt(60)
+# fd(120)
+# lt(60)
+# fd(120)
+# lt(60)
+# fd(120)
+# lt(60)
+# fd(120)
+# lt(60)
+
+for i in range(1,200):
+    fd(10)
+    lt(10)
+
+hideturtle()
+mainloop()
