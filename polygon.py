c0aeefa832de6e00a4acd7c23b00911fb0887bbc
gupta-aayushkr <gupta.aayushkr@gmail.com>
2023-08-11 20:03:41 +0530

    pgrun game

---
+++ polygon.py
@@ -0,0 +1,50 @@
+from turtle import *
+
+def triangle():
+    for i in range(3):
+        fd(100)
+        lt(360/3)
+
+def sqare():
+    for i in range(4):
+        fd(100)
+        lt(360/4)
+
+def pentagon():
+    for i in range(5):
+        fd(100)
+        lt(360/5)
+
+def hexagon():
+    for i in range(6):
+        fd(100)
+        lt(360/6)
+
+def heptagon():
+    for i in range(7):
+        fd(100)
+        lt(360/7)
+
+def octagon():
+    for i in range(8):
+        fd(100)
+        lt(360/8)
+
+def nonagon():
+    for i in range(9):
+        fd(100)
+        lt(360/9)
+
+def decagon():
+    for i in range(10):
+        fd(100)
+        lt(360/10)
+
+# def allgon():
+#     for i in range(n):
+#         fd(100)
+#         lt(360/n)
+
+print(__name__)
+if __name__ == "__main__":
+    print("This is runnning as current file")
---
+++ t3.py
@@ -0,0 +1,17 @@
+from polygon import *
+
+# triangle()
+# sqare()
+# pentagon()
+# hexagon()
+# heptagon()
+# octagon()
+# nonagon()
+# decagon()
+
+for i in range(1,11):
+    triangle()
+    fd(100)
+    lt(360/10)
+
+mainloop()
